88e2d09e962feeec457de090f26ecf2051064b94
Reny <reny.dev03@gmail.com>
2026-05-24 15:56:59 -0300

    Adiciona top10_melhores_notas

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import csv
 HQs = []
 
-with open("HQs.csv", mode="r") as arq:
+with open("HQs.csv", mode="r", encoding="utf-8") as arq:
     dados_csv = csv.DictReader(arq)
     for linha in dados_csv:
         HQs.append(linha)
@@ -10,6 +10,16 @@ def titulo(texto):
     print()
     print(texto)
     print("-"*40)
+
+def top10_melhores_notas():
+    titulo("Top 10 HQs com as melhores notas")
+
+    notas = [x for x in HQs]
+    ordenar = sorted(notas, key=lambda hq: float(hq['Rating']), reverse=True)
+
+    for x, hq in enumerate(ordenar[0:10], start=1):
+        nota = float(hq['Rating'])
+        print(f"{x:2d} {hq['Title']:20s} {hq['Studio/Publisher']:20s} {nota:4.1f} {hq['comic_id']}")
 
 while True:
     titulo("Jogadores do brazileirão")
